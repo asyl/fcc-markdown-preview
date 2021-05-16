de39083bbedbf0c20fcaf2da49b1407f2a7609c4
Asil Isakov <asyl.dev@gmail.com>
2021-05-16 18:15:29 +0200

    install `marked.js` for markdown & implement in Preview component

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,5 +1,28 @@
+import PropTypes from "prop-types";
+import marked, { Renderer } from "marked";
+
 const Preview = ({ markdown }) => {
-  return <div id="preview" />;
+  marked.setOptions({
+    breaks: true,
+  });
+
+  const renderer = new Renderer();
+  renderer.link = function (href, title, text) {
+    return `<a target="_blank" href="${href}">${text}</a>;`
+  };
+
+  return (
+    <div
+      id="preview"
+      dangerouslySetInnerHTML={{
+        __html: marked(markdown, { renderer: renderer }),
+      }}
+    />
+  );
+};
+
+Preview.propTypes = {
+  markdown: PropTypes.string.isRequired,
 };
 
 export default Preview;
